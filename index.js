b97101dce298bf498e946b282734fbcba7cda6a8
vedantyadu <vedantyadu@gmail.com>
2023-02-05 19:13:53 +0530

    Added dotenv

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express")
 const topic = require("./routes/topic")
 const search = require("./routes/search")
 const test = require("./routes/test")
+const dotenv = require("dotenv").config()
 const app = express()
-const PORT = 3000
+
+const PORT = process.env.PORT
 
 app.use("/search", search)
 app.use("/topic", topic)
 app.use("/", test)
 
-app.listen(PORT, () => {console.log("Listening on 3000")})
+app.listen(PORT, () => {console.log("Listening on " + PORT)})
